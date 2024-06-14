8245e6d6a9a4641e9d90d6c71e8f1c6752f6f928
Dtnkcreator <dtnk56@gmail.com>
2024-06-14 15:08:53 -0300

    att 2

---
+++ estudo_modulos/hello.py
@@ -0,0 +1,14 @@
+def world():
+
+    print("Hello, World!")
+
+shark = "Baby Shark"
+
+class Octupus:
+    def __init__(self, name, color):
+        self.color = color
+        self.name = name
+
+    def tell_me_about_the_octupus(self):
+        print("This octopus is " + self.color + ".")
+        print(self.name + " is the octopus name.")
---
+++ estudo_modulos/main_program.py
@@ -0,0 +1,7 @@
+import hello
+
+hello.world()
+
+polvo = hello.Octupus("Sam", "Yellow")
+
+polvo.tell_me_about_the_octupus()
